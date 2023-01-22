2b131f4574e5cd987dac77a605a02d81ddc5929f
Eldin Kecap <eldinkecap@gmail.com>
2023-01-22 13:20:25 +0100

    Started layouts and nested routes exercise

--- react-complete-guide/ReactRouter/01-starting-project/src/App.js
+++ react-complete-guide/ReactRouter/01-starting-project/src/App.js
@@ -1,9 +1,20 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements , Route} from 'react-router-dom'
 import HomePage from './pages/Home';
+import ProductsPage from './pages/ProductsPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <HomePage /> },
+  { path: '/products', element:<ProductsPage/> }
 ])
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<HomePage></HomePage>}/>
+//     <Route path='/products' element={<ProductsPage></ProductsPage>}/>
+//   </Route>
+// ) 
+
+// const router = createBrowserRouter(routeDefinitions)
 
 
 function App() {
--- react-complete-guide/ReactRouter/01-starting-project/src/pages/Home.js
+++ react-complete-guide/ReactRouter/01-starting-project/src/pages/Home.js
@@ -1,5 +1,11 @@
+import { Link } from "react-router-dom"
+
 function HomePage(){
-    return <h1>My home page</h1>
+    return <>
+    <h1>My home page</h1>
+    <p>Go to list of </p>
+    <Link to='/products' >products</Link>
+    </>
 }
 
 export default HomePage
