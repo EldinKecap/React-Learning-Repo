e7914eeea642caabf0d7c10513fed1df5475420d
Eldin Kecap <eldinkecap@gmail.com>
2023-01-25 02:12:59 +0100

    Completed Layouts and Nested Routes exercise

--- react-complete-guide/ReactRouter/01-starting-project/src/App.js
+++ react-complete-guide/ReactRouter/01-starting-project/src/App.js
@@ -1,10 +1,17 @@
-import { createBrowserRouter, RouterProvider, createRoutesFromElements , Route} from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
 import HomePage from './pages/Home';
 import ProductsPage from './pages/ProductsPage';
+import RootLayout from './pages/Root';
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element:<ProductsPage/> }
+  {
+    path: '/', element: <RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> }
+    ]
+  }
+
 ])
 
 // const routeDefinitions = createRoutesFromElements(
