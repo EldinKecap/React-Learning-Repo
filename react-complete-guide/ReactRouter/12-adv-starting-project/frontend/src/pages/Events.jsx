d11dd4e2b691d67c6182947074c9bfba834166d1
Eldin Kecap <eldinkecap@gmail.com>
2023-02-01 21:12:37 +0100

    finished react router module

--- react-complete-guide/ReactRouter/12-adv-starting-project/frontend/src/pages/Events.jsx
+++ react-complete-guide/ReactRouter/12-adv-starting-project/frontend/src/pages/Events.jsx
@@ -1,28 +1,32 @@
 
-import { json, useLoaderData } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Await, defer, json, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
-  const data = useLoaderData();
-  // if (data.isError) {
-  //   return <p>{data.message}</p>
-  // }
-  const events = data.events;
-  return (
-    <>
-      {<EventsList events={events} />}
-    </>
-  );
+  const { events } = useLoaderData();
+
+  return <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+    <Await resolve={events}>
+      {(loadedEvents) => <EventsList events={loadedEvents}></EventsList>}
+    </Await>
+  </Suspense>
 }
 
 export default EventsPage;
 
-export async function loader() {
+export async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
   if (!response.ok) {
     // return { isError: true, message: 'Could not fetch events' };
-    throw json({message: 'Could not fetch events.'},{status:500});
+    throw json({ message: 'Could not fetch events.' }, { status: 500 });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+
+export function loader() {
+  return defer({ events: loadEvents() })
 }
