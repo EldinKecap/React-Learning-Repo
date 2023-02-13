fce1a894b8e70c84db0f425b094a3c961762d1f2
Eldin Kecap <eldinkecap@gmail.com>
2023-02-13 21:42:58 +0100

    Added lazy loading

--- react-complete-guide/ReactRouter/12-adv-starting-project/frontend/src/App.js
+++ react-complete-guide/ReactRouter/12-adv-starting-project/frontend/src/App.js
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import EditEventPage from './pages/EditEvent';
 import ErrorPage from './pages/Error';
-import EventDetailPage, {
+import {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from './pages/EventDetail';
@@ -13,6 +13,10 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
+import React from 'react';
+
+const EventDetailPage = React.lazy(() => import('./pages/EventDetail'));
+
 
 const router = createBrowserRouter([
   {
@@ -37,7 +41,7 @@ const router = createBrowserRouter([
             children: [
               {
                 index: true,
-                element: <EventDetailPage />,
+                element: <React.Suspense fallback={<p style={{textAlign:'center'}}>Loading...</p>}><EventDetailPage /></React.Suspense>,
                 action: deleteEventAction,
               },
               {
